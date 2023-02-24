ce0baa4283f9cea1a1dfee9a06d5d2fde66e7f6c
Saraf Nawar <s.999895831@gmail.com>
2023-02-24 00:16:18 -0500

    tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -19,21 +19,53 @@ router.get('/', (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-});
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [ 
+        {model: Product}
+      ],
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that ID'});
+      returns; 
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err); 
+  }
+  });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then((tag) => res.status(200).json(tag))
+  .catch((err) => res.status(404).json(err));
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((tag) => res.status(200).json(tag))
+  .catch((tag) => res.status(404).json(tag));
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy(req.body, {
+    where: {
+      id: req.params.id,
+    }
+  })
+  .then((tag) => res.status(200).json(tag))
+  .catch((tag) => res.status(404).json(tag));
 });
+
 
 module.exports = router;
