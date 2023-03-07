6a4d22d403de9bdd7b04c47c27739d665b28d76a
Saraf Nawar <s.999895831@gmail.com>
2023-03-07 14:29:31 -0500

    changed db name

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -46,7 +46,6 @@ router.get('/:id', async (req, res) => {
 // create new product
 router.post('/', (req, res) => {
   Product.create({
-
     product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
